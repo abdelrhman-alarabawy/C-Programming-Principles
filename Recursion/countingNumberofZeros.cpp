3ac71c76f178cecfa5bdca02a932032e94634db9
Abdelrhman Alarabawy <83235049+abdelrhman-alarabawy@users.noreply.github.com>
2024-02-23 23:28:47 +0200

    Update countingNumberofZeros.cpp

--- Recursion/countingNumberofZeros.cpp
+++ Recursion/countingNumberofZeros.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+using namespace std;
 
+int countZero(int n){
+    if(n == 0){
+        return 1;
+    }
+    else if(n < 10){
+        return 0;
+    }
+    else if(n%10 == 0){
+        return (countZero(n/10) + 1);
+    }
+    else{
+        return countZero(n/10);
+    }
+}
+
+int main(){
+    int n,count;
+    cout<<"Enter your number"<<endl;
+    cin>>n;
+    count = countZero(n);
+    cout<<"Number of zeros in your number "<<n<<" equals "<<count;
+    return 0;
+}
